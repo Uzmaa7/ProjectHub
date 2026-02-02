b5aa08b4714ad68a9b0f397c61273d74300e19b1
Uzmaa7 <uk9971khan@gmail.com>
2026-02-03 00:10:52 +0530

    add custom schema methods for Bcrypt comparison, Access/Refresh tokens, and temporary crypto tokens

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from "crypto"
+
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -74,6 +77,8 @@ const userSchema = new mongoose.Schema({
 }, {timestamps:true});
 
 
+// ----------------------------------------- //
+
 userSchema.pre("save", async function(next){
     if(!this.isModified("password"))return next();
     this.password = await bcrypt.hash(this.password, 10);
@@ -81,6 +86,56 @@ userSchema.pre("save", async function(next){
 })
 
 
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+
+userSchema.methods.generateAccesstoken = function(){
+    return jwt.sign(
+            {
+                _id : this._id,
+                email: this.email,
+                username: this.username
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+            }
+    )
+}
+
+
+userSchema.methods.generateRefreshtoken = function(){
+    return jwt.sign(
+            {
+                _id : this._id,
+                email: this.email,
+                username: this.username
+            },
+            process.env.Refresh_TOKEN_SECRET,
+            {
+                expiresIn: process.env.Refresh_TOKEN_EXPIRY
+            }
+    )
+}
+
+
+userSchema.methods.generateTemporaryToken = function(){
+    const unHashedToken = crypto.randomBytes(20).toString("hex");
+
+    const HashedToken = crypto
+                        .createHash("sha26")
+                        .update(unHashedToken)
+                        .digest("hex")
+
+    const tokenExpiry = Date.now() + (20*60*1000)
+
+    return {HashedToken, tokenExpiry};
+}
+
+
+// -------------------------------------------- //
 
 const User = mongoose.model("User", userSchema);
 
