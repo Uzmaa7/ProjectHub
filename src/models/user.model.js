86c8cd21d95b09bef2e7c8358b8e8d1fa2bf9c49
Uzmaa7 <uk9971khan@gmail.com>
2026-02-02 22:17:46 +0530

    update user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,76 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({}, {timestamps:true});
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required:true,
+        lowercase: true,
+        unique: true,
+        index: true,
+        trim: true
+    },
+
+    fullname: {
+        type: String,
+        required:true,
+    },
+
+    avatar: {
+        type: {
+            url: String,
+            localpath: String
+        },
+        default: {
+            url: `https://placehold.co/600x400`,
+            localpath: ""
+        }
+    },
+
+    email: {
+        type: String,
+        required:true,
+        lowercase: true,
+        unique: true,
+        trim: true
+
+    },
+
+    password: {
+        type: String,
+        required:true,
+        min: [8, "must be 8 characters long"]   
+    },
+
+    emailVerificationToken: {
+        type: String
+    },
+
+    emailVerificationTokenExpiry: {
+        type: Date
+    },
+
+    isEmailVerified: {
+        type: Boolean,
+        default: false
+    },
+
+    refreshToken:{
+        type: String
+    },
+
+
+    forgotPasswordToken: {
+        type: String,
+    },
+
+    forgotPasswordTokenExpiry: {
+        type: Date,
+    },
+
+
+
+    
+}, {timestamps:true});
 
 
 const User = mongoose.model("User", userSchema);
